612e25933cf723268cda296cd87a2cee9da8c5c5
Julian <julisanchez1991@gmail.com>
2020-02-03 18:32:55 -0300

    segundo commit 3/2/2020 18:32 Header creation

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Proyecto Landing Page Trivago(https://github.com/dondarrion91/trivago_landing).
 
 Inicializacion del esqueleto de la app.
 
+## Creacion del header de la pagina
+
+Para el Header cree un componente llamado Header en el archivo header.js y lo importe a el script de la app principal donde hice uso de bootstrap para los estilos ya que se asemejan al de la pagina original y es mucho mas rapido.
+
---
+++ src/components/header.js
@@ -0,0 +1,47 @@
+import React , {Component} from 'react';
+
+
+export default class Header extends Component{
+    render(){
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light ">
+
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Iniciar Sesión</a>
+                        </li>
+
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Menú</a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a className="dropdown-item" href="#">Action</a>
+                                <a className="dropdown-item" href="#">Another action</a>
+                                <a className="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                               ARS</a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a className="dropdown-item" href="#">Action</a>
+                                <a className="dropdown-item" href="#">Another </a>
+                                <a className="dropdown-item" href="#">Something</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+
+    
+
+} 
+
